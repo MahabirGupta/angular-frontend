864370933224abc6042ea27073487196d6aac41d
MahabirGupta <mahabirg81@gmail.com>
2024-01-16 09:55:03 +0800

    Employee List

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
+  // standalone: true,
   // imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
