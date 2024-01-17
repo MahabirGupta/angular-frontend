aee927d026a7ed7f71a72e6795029696d82182cc
MahabirGupta <mahabirg81@gmail.com>
2024-01-17 10:47:41 +0800

    Import Common Module and get employees from database

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -16,7 +16,20 @@ export class EmployeeListComponent implements OnInit {
   constructor(){}
 
   ngOnInit(): void {
-    
+    this.employees=[
+      {
+      "id": 1,
+      "firstName": "Mahabir",
+      "lastName": "Gupta",
+      "emailId": "mahabir@gmail.com"
+      },
+      {
+      "id": 2,
+      "firstName": "Anish",
+      "lastName": "Gupta",
+      "emailId": "anish@gmail.com"
+      }
+      ]
   }
 
 }
--- src/app/employee-list/employee-list.module.ts
+++ src/app/employee-list/employee-list.module.ts
@@ -2,9 +2,11 @@
 
 import { NgModule } from '@angular/core';
 import { EmployeeListComponent } from './employee-list.component';
+import { CommonModule } from '@angular/common'; // Import CommonModule
 
 @NgModule({
   declarations: [EmployeeListComponent],
+  imports: [CommonModule], // Include CommonModule here
   exports: [EmployeeListComponent],
 })
 export class EmployeeListModule {}
